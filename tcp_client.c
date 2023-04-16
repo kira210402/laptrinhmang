dbacbee2986501f4fd2d21418e200a6e027537ef
kira210402 <duongkimnam2142002@gmail.com>
2023-04-16 21:16:19 +0700

    laptrinhmang

---
+++ tcp_client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+     
+    printf("ket noi den may chu ip: %s , port: %d \n", ip, port);
+
+    int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
+
+    int ret = connect(client, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret == -1)
+    {
+        perror("connect() failed");
+        return 1;
+    }
+
+    char buf[256];
+
+    printf("bat dau gui du lieu, nhap 'exit' de ket thuc\n");
+
+    while (1)
+    {
+        printf("Enter string:");
+        fgets(buf, sizeof(buf), stdin);
+
+        send(client, buf, strlen(buf), 0);
+
+        if (strncmp(buf, "exit", 4) == 0)
+            break;
+    }
+
+    close(client);
+}
